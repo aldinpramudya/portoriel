c1a82e86b78e2325e1925d28255b3f697a463ef7
aldinpramudya <aldinarielpramudya@gmail.com>
2024-05-23 11:56:23 +0700

    add : responsive mobile

--- scripts/main.js
+++ scripts/main.js
@@ -2,4 +2,17 @@ window.onbeforeunload = () => {
   for (const form of document.querySelectorAll("form")) {
     form.reset()
   }
-}
+};
+
+// Tooggle Button Hamburger
+const navSlide = () => {
+  const burger = document.querySelector('.burger');
+  const navLists = document.querySelector('nav');
+
+  burger.addEventListener('click', () => {
+    navLists.classList.toggle('nav-active');
+    burger.classList.toggle('toggle-burger');
+  });
+};
+
+navSlide();
